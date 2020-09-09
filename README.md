84ee36089fbad179777faaacaebdd2c3db7db2ba
TrvsF <traviseff@gmail.com>
2020-09-09 18:01:26 +0100

    src

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# homepage
+internet browser landing page
+
+majority stolen from here : https://github.com/Jaredk3nt/homepage
+
+posted so i can add it to my laptop faster (i change distros a lot)
---
+++ bookmarks.js
@@ -0,0 +1,30 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [{
+        title: 'Daily',
+        links: [
+            { name: 'Gmail', url: 'https://mail.google.com/mail/u/1/#inbox' },
+            { name: 'GitHub', url: 'https://github.com' },
+            { name: 'YouTube', url: 'https://youtube.com' }
+        ]
+    },
+    {
+        title: 'School',
+        links: [
+            { name: ':(', url: 'need.to.add.this.manually.every.time.so.a.small.asian.kid.doesnt.ring.my.school.telling.them.im.a.[redacted].bc.i.token.logged.him.and.leaked.his.shitty.client.fun' }
+        ]
+    },
+    {
+        title: 'Boards',
+        links: [
+            { name: '/wg/', url: 'https://boards.4chan.org/wg/' },
+            { name: '/mu/', url: 'https://boards.4chan.org/mu/' },
+            { name: '/fa/', url: 'https://boards.4chan.org/fa/' }
+        ]
+    },
+    {
+        title: 'Social',
+        links: [
+            { name: 'Discord', url: 'https://discord.com' }
+        ]
+    }
+];
